4e858c48465bc4a16b9f7887250feb5cf18628c9
AntMan <xanthonyalvarezx@gmail.com>
2020-06-16 15:44:48 -0400

    beautified all files and added comments to main.js

--- main.js
+++ main.js
@@ -1,55 +1,60 @@
 body = document.querySelector('body')
 
-
+//main removal button
 const mainButton = document.createElement('button')
 mainButton.append('Remove main')
-mainButton.id ='mainButton'
+mainButton.id = 'mainButton'
 
 
-//button2
+//remove button
 const button2 = document.createElement('button')
 button2.append('remove')
+//create button
 const button3 = document.createElement('button')
 button3.append('create')
-//input
+//input for remove
 const input = document.createElement('input')
 input.type = 'text'
 input.name = 'remove'
 input.id = 'remove'
 input.placeholder = 'remove selector here...'
-//input2
+//input for create
 const input2 = document.createElement('input')
 input2.type = 'text'
 input2.name = 'create'
 input2.id = 'create'
 input2.placeholder = 'enter new text here...'
-
+// main appends
 const main = document.createElement('main')
-body.append(button2,' ',button3,' ', mainButton, main, input, ' ', input2)
-
+body.append(button2, ' ', button3, ' ', mainButton, main, input, ' ', input2)
+//anchor tag 
 const anchor = document.createElement('a')
 anchor.className = 'link'
 anchor.href = 'https://www.facebook.com/photo/?fbid=10157599456014005&set=a.10150981369904005'
 anchor.target = '_blank'
 anchor.append('My Mazda 2 and my Facebook')
+//image tag
 const image = document.createElement('img')
 image.className = 'image'
 image.src = 'Mazda.jpg'
+//main appends
 main.append(image, anchor)
-
+// main button event listner
 mainButton.addEventListener('click', function () {
- 
-main.remove()
+
+    main.remove()
 })
+//remove button event listner
 button2.addEventListener('click', function () {
-let selector = document.querySelector(input.value) 
-selector.remove()
+    let selector = document.querySelector(input.value)
+    selector.remove()
 })
 
+//create button event listner
 button3.addEventListener('click', function () {
-let addition = input3.value
-newDiv = document.createElement('div')
-body.append(newDiv)
-newDiv.append(addition)
-console.log(addition)
+    let addition = input3.value
+    newDiv = document.createElement('div')
+    body.append(newDiv)
+    newDiv.append(addition)
+    console.log(addition)
 })
